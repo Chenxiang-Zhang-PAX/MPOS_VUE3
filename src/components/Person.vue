0b735aa4bb34200d688ab0eceea771ff7aefe6f7
Chenxiang Zhang (PAX) <chenxiang.zhang@pax.us>
2024-01-04 17:44:44 -0500

    first commit

---
+++ src/components/Person.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>Person Info</div>
+        <div>Name: {{ person.name }}</div>
+        <div>Age: {{ person.age }}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { type Person } from '@/types';
+
+
+
+const p: Person = {
+    id: '#001',
+    name: 'Bill',
+    age: 20
+}
+
+const person = ref(p)
+</script>
